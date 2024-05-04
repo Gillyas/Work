dd03038038f04d8131f3b2adfc8a800a6b1895f4
Gillyas <155540907+Gillyas@users.noreply.github.com>
2024-05-04 18:07:51 +0400

    First commit

---
+++ app/src/main/java/com/example/work/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.work
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.work.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+    fun onClickToast(view: View) {
+        Toast.makeText(this, "Я буду android-разработчиком!!", Toast.LENGTH_SHORT).show() // создали функцию с нашей кнопкой
+
+    }
+
+}
